30fd78a3bde10720501bc4a43080920e6ca69b11
Max Bogue <max@maxbogue.com>
2017-06-07 17:06:51 -0700

    Render all claims on home page.

--- src/DwdHome.vue
+++ src/DwdHome.vue
@@ -1,20 +1,33 @@
 <template>
 <div>
-  Welcome!
+  <router-link v-for="(claim, id) in claims"
+               class="claim"
+               :to="'/' + id"
+               :key="id">
+    {{ claim.text }}
+  </router-link>
 </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  props: [],
   data: () => ({
   }),
-  computed: {
-  },
+  computed: mapState(['claims']),
   methods: {
   },
 };
 </script>
 
 <style>
+a.claim {
+  color: #000;
+  display: block;
+}
+a.claim:hover {
+  background-color: #E0E0E0;
+  text-decoration: none;
+}
 </style>
