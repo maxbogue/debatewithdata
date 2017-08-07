0b2dcd31d307c0f4a527e9350d1856962a5d1890
Max Bogue <max@maxbogue.com>
2017-08-07 13:19:58 -0700

    Improve home page. Contributes to #12.

--- src/DwdHome.vue
+++ src/DwdHome.vue
@@ -1,22 +1,86 @@
 <template>
-<div class="center">
-  <div class="construction against">Under construction!</div>
+<div class="center home">
+  <h1>DebateWithData</h1>
+  <h2>A crowdsourced reference for the world's discussions.</h2>
+  <hr></hr>
+  <h3>Have informed debates without writing your own research paper!</h3>
+  <div class="guide b">
+    <h3>Learn</h3>
+    <ul>
+      <li>Find a claim you want to know more about.</li>
+      <li>Read the points for that claim to understand both sides of it.</li>
+      <li>Dive into the sources to see the data behind each side.</li>
+    </ul>
+  </div>
+  <div class="guide r">
+    <h3>Talk</h3>
+    <ul>
+      <li>If you find yourself in a discussion, find the claim that best
+        represents the disagreement you're having.</li>
+      <li>Try to stay on the topic of that specific claim.</li>
+      <li>Use what you learn from the claim page to strengthen your points with
+        data and call out the weaknesses in theirs.</li>
+    </ul>
+  </div>
+  <div class="guide g">
+    <h3>Contribute</h3>
+    <ul>
+      <li>Add sources that contribute relevant data to a discussion. The closer
+        to the origin of the data, the better.</li>
+      <li>Add claims you've encountered people making, regardless of whether
+        you agree with them personally.</li>
+      <li>Star any claims and points that you think are relevant, compelling,
+        and supported by data.</li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
-<style>
-.center {
-  text-align: center;
+<style scoped>
+.home h1 {
+  font-size: 48px;
+  margin: 20px 0 8px;
 }
-.construction {
-  display: inline-block;
+.home h2 {
   font-size: 24px;
-  margin-top: 16px;
-  padding: 16px;
+  font-weight: 400;
+  margin: 8px 0;
+}
+.home h3 {
+  font-size: 20px;
+}
+.guide {
+  border-radius: 5px;
+  display: block;
+  margin: 20px 0;
+  padding: 20px;
+  text-align: left;
+}
+.guide.r {
+  background-color: #FFEBEE;
+  border: 1px solid #FFCDD2;
+}
+.guide.g {
+  background-color: #E8F5E9;
+  border: 1px solid #C8E6C9;
+}
+.guide.b {
+  background-color: #E3F2FD;
+  border: 1px solid #BBDEFB;
+}
+.guide > h3 {
+  font-weight: bold;
+  margin: 0 0 8px;
+}
+.guide ul {
+  margin: 0;
+  padding-left: 16px;
+}
+.guide li {
+  margin-top: 4px;
 }
 </style>
