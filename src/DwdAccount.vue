b89166d79df763db482333c205b4387f0cec9d69
Max Bogue <max@maxbogue.com>
2017-10-25 04:01:36 -0700

    Slightly prettier account page.

--- src/DwdAccount.vue
+++ src/DwdAccount.vue
@@ -1,22 +1,40 @@
 <template>
-<div class="center">
-  <div class="for construction">
-    <p>Username: {{ user.username }}</p>
-    <p>Created on: {{ user.created.toDateString() }}</p>
-    <p>Email address: {{ user.email }}</p>
-  </div>
+<div class="account center">
+  <div><span>user</span><span>{{ user.username }}</span></div>
+  <div><span>created</span><span>{{ created }}</span></div>
+  <div><span>email</span><span>{{ user.email }}</span></div>
 </div>
 </template>
 
 <script>
+import dateFormat from 'dateformat';
 import { mapState } from 'vuex';
 
 export default {
-  computed: mapState([
-    'user',
-  ]),
+  computed: {
+    ...mapState(['user']),
+    created: function () {
+      return dateFormat(this.user.created, 'yyyy-mm-dd');
+    },
+  },
 };
 </script>
 
 <style>
+.account {
+  margin-top: 50px;
+}
+.account span {
+  display: inline-block;
+  font-size: 1.5em;
+  font-weight: 100;
+  padding: 4px;
+  text-align: left;
+  width: 60%;
+}
+.account span:first-child {
+  font-weight: 500;
+  text-align: right;
+  width: 40%;
+}
 </style>
