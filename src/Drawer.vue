f4b71f0f89b27aa1bae9348e5cf8e4ba05bdaf2e
Max Bogue <max@maxbogue.com>
2017-11-16 07:04:28 -0800

    Slide down sub-points on click.

---
+++ src/Drawer.vue
@@ -0,0 +1,33 @@
+<template>
+<transition name="drawer">
+  <div class="drawer">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</transition>
+</template>
+
+<style lang="sass">
+.drawer
+  max-height: 300px
+  overflow: scroll
+
+.drawer-enter-active,
+.drawer-leave-active,
+.drawer-enter-active > div,
+.drawer-leave-active > div
+  transition: all 0.3s
+
+.drawer.drawer-enter,
+.drawer.drawer-leave-to
+  max-height: 0
+
+.drawer-enter > div,
+.drawer-leave-to > div
+  transform: translateY(-100%)
+
+.drawer-leave > div,
+.drawer-enter-to > div
+  transform: translateY(0)
+</style>
--- src/DwdPoint.vue
+++ src/DwdPoint.vue
@@ -1,31 +1,34 @@
 <template>
 <div class="point" :class="isFor | toSideString">
-  <div class="bubble">
+  <div class="bubble click" @click="showDrawer = !showDrawer">
     <point-content class="content"
                    :point="point"
                    :trail="trail"></point-content>
-    <div class="controls">
+  </div>
+  <drawer v-if="showDrawer">
+    <div class="info">
       <dwd-star :star="point.star" :url="'/api/point/' + point.id"></dwd-star>
       <span class="glyphicon glyphicon-comment click"
             aria-hidden="true"
             @click="showComments = !showComments"></span>
     </div>
-  </div>
-  <dwd-comments v-if="showComments"
-                :url="'/api/point/' + point.id"></dwd-comments>
-  <ul v-if="subPoints.length > 0">
-    <dwd-sub-point v-for="[subPoint, subSide, i] in subPoints"
-                   :point="subPoint"
-                   :isFor="isFor === !subSide"
-                   :trail="trail.concat(point.claimId || point.id)"
-                   :key="subPoint.id">
-    </dwd-sub-point>
-  </ul>
+    <dwd-comments v-if="showComments"
+                  :url="'/api/point/' + point.id"></dwd-comments>
+    <ul v-if="subPoints.length > 0" class="subpoints">
+      <dwd-sub-point v-for="[subPoint, subSide, i] in subPoints"
+                     :point="subPoint"
+                     :isFor="isFor === !subSide"
+                     :trail="trail.concat(point.claimId || point.id)"
+                     :key="subPoint.id">
+      </dwd-sub-point>
+    </ul>
+  </drawer>
 </div>
 </template>
 
 <script>
 import './style/point.sass';
+import Drawer from './Drawer.vue';
 import DwdComments from './DwdComments.vue';
 import DwdStar from './DwdStar.vue';
 import DwdSubPoint from './DwdSubPoint.vue';
@@ -34,6 +37,7 @@ import { pointMapsToLists, rotateWithIndexes } from './utils';
 
 export default {
   components: {
+    Drawer,
     DwdComments,
     DwdStar,
     DwdSubPoint,
@@ -42,6 +46,7 @@ export default {
   props: ['point', 'isFor', 'trail'],
   data: () => ({
     showComments: false,
+    showDrawer: false,
   }),
   computed: {
     sortedSubpoints: function () {
