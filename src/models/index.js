7b63f60252f6d2f7cde82a061443a5533be4effd
Max Bogue <max@maxbogue.com>
2019-07-21 10:39:08 -0700

    Update deps to fix security vulnerabilities.

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ pg.types.setTypeParser(20, 'text', parseInt);
 
 import config from 'config';
 import Knex from 'knex';
-import KnexQueryBuilder from 'knex/src/query/builder';
+import KnexQueryBuilder from 'knex/lib/query/builder';
 import _ from 'lodash/fp';
 import Sequelize from 'sequelize';
 
